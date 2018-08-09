55dd37362a8b8592fdb4089daa460a7cbe509bd0
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-08-09 16:14:27 -0300

    Corrected details in user model

--- app/models/user.js
+++ app/models/user.js
@@ -2,22 +2,11 @@
 
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-
     id:        { type: Sequelize.INTEGER, allowNull: false, unique: true, autoIncrement: true },
-
     firstName: { type: Sequelize.STRING, allowNull: false },
-
     lastName:  { type: Sequelize.STRING, allowNull: false },
-
     email:     { type: Sequelize.STRING, allowNull: false, unique: true },
-
     password:  { type: Sequelize.STRING, allowNull: false, autoIncrement: true }
-
   }, {});
-
-  User.associate = function(models) {
-    // associations can be defined here
-  };
   return User;
-
 };
