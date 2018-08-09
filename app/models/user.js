2863e9e375f4542d19011b2a42881e929e7447ec
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-08-09 17:03:46 -0300

    Errors were corrected to run npm start

--- app/models/user.js
+++ app/models/user.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    id:        { type: Sequelize.INTEGER, allowNull: false, unique: true, autoIncrement: true },
-    firstName: { type: Sequelize.STRING, allowNull: false },
-    lastName:  { type: Sequelize.STRING, allowNull: false },
-    email:     { type: Sequelize.STRING, allowNull: false, unique: true },
-    password:  { type: Sequelize.STRING, allowNull: false, autoIncrement: true }
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false, unique: true, autoIncrement: true },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false }
+    },
+    {}
+  );
   return User;
 };
