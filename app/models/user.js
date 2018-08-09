5feb19a005e4c910e28682589f6cae0a1df90c94
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-08-09 15:35:34 -0300

    The user model was created and a configuration error was fixed in .env

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,23 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+
+    id:        { type: Sequelize.INTEGER, allowNull: false, unique: true, autoIncrement: true },
+
+    firstName: { type: Sequelize.STRING, allowNull: false },
+
+    lastName:  { type: Sequelize.STRING, allowNull: false },
+
+    email:     { type: Sequelize.STRING, allowNull: false, unique: true },
+
+    password:  { type: Sequelize.STRING, allowNull: false, autoIncrement: true }
+
   }, {});
+
   User.associate = function(models) {
     // associations can be defined here
   };
   return User;
+
 };
