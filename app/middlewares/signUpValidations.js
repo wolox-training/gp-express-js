a31ed44b3402dbfc91ccd0b857877ce9bfad37f6
Gabriel Pasquale <pasqualeunq@gmail.com>
2018-08-13 13:43:00 -0300

    SignUp tests were added

--- app/middlewares/signUpValidations.js
+++ app/middlewares/signUpValidations.js
@@ -3,8 +3,8 @@ const User = require('../models').User,
 
 exports.validate = (req, res, next) => {
   const errors = [];
-  const isWoloxValidEmail = req.body.email.endsWith('@wolox.com.ar');
-  const isPasswordValid = req.body.password.length >= 8;
+  const isWoloxValidEmail = req.body.email && req.body.email.endsWith('@wolox.com.ar');
+  const isPasswordValid = req.body.password && req.body.password.length >= 8;
   if (!isWoloxValidEmail) errors.push(error.invalidMail.message);
   if (!isPasswordValid) errors.push(error.invalidPassword.message);
   User.findOne({
